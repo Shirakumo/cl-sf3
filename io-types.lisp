0cbe3dcdcff26f6d6dac829e366c9858b38d4e75
Shinmera <shinmera@tymoon.eu>
2023-04-22 19:59:37 +0200

    Fixes and print-object and such.

--- io-types.lisp
+++ io-types.lisp
@@ -6,11 +6,23 @@
 
 (in-package #:org.shirakumo.sf3)
 
+(defun format-time (&optional (timestamp (get-universal-time)))
+  (multiple-value-bind (s m h dd mm yy) (decode-universal-time (+ timestamp (encode-universal-time 0 0 0 1 1 1970 0)))
+    (format NIL "~4,'0d-~2,'0d-~2,'0d ~2,'0d:~2,'0d:~2,'0d" yy mm dd h m s)))
+
+(defmacro define-print-method (class format &rest format-args)
+  `(defmethod print-object ((object ,class) stream)
+     (print-unreadable-object (object stream :type T)
+       (format stream ,format ,@(loop for arg in format-args
+                                      collect (if (symbolp arg) `(slot-value object ',arg) arg))))))
+
 (bs:define-io-structure archive-meta-entry
   (modification-time uint64)
   (checksum uint32)
   (mime-type (string uint8))
   (path (string uint16)))
+
+(define-print-method archive-meta-entry "~a ~a" mime-type path)
 
 (bs:define-io-structure archive
   (count uint64)
@@ -19,6 +31,8 @@
   (meta-entries (vector archive-meta-entry (bs:slot count) (aref (bs:slot meta-offsets) bs:i)))
   (file-offsets (vector uint64 (bs:slot count)))
   (files (vector (vector uint8 uint64) (bs:slot count) (aref (bs:slot file-offsets) bs:i))))
+
+(define-print-method archive "~d" count)
 
 (bs:define-io-alias format
     (case uint8
@@ -51,6 +65,8 @@
                      (#x24 float32)
                      (#x28 float64)))))
 
+(define-print-method audio "~dx~a @~dHz" channels format samplerate)
+
 (bs:define-io-structure image
   (width uint32)
   (height uint32)
@@ -60,8 +76,25 @@
   (data (vector uint8 (* (bs:slot width)
                          (bs:slot height)
                          (bs:slot depth)
-                         (bs:slot channels)
+                         (ldb (byte 4 0) (bs:slot channels))
                          (bs:octet-size (bs:slot format))))))
+
+(defun pixel-type (image)
+  (ecase (image-channels image)
+    (#x01 :K)
+    (#x02 :KA)
+    (#x03 :RGB)
+    (#x04 :RGBA)
+    (#x12 :AK)
+    (#x13 :BGR)
+    (#x14 :ABGR)
+    (#x24 :ARGB)
+    (#x34 :BGRA)
+    (#x44 :CMYK)
+    (#x54 :KYMC)))
+
+(define-print-method image "~d~[~;~:;~:*x~d~]~[~;~:;~:*x~d~] ~a ~a" 
+  width height depth format (pixel-type object))
 
 (bs:define-io-structure log-entry
   (size uint32)
@@ -71,25 +104,60 @@
   (category (string uint8))
   (message (string uint16)))
 
+(define-print-method log-entry "~a ~d ~a ~a ~a"
+  (format-time (log-entry-time object)) severity source category message)
+
 (bs:define-io-structure log-chunk
   (size uint64)
   (entry-count uint32)
   (entry-offsets (vector uint64 (bs:slot entry-count)))
   (entries (vector log-entry (bs:slot entry-count) (aref (bs:slot entry-offsets) bs:i))))
 
+(define-print-method log-chunk "~d" entry-count)
+
 (bs:define-io-structure log
   (start-time uint64)
   (chunk-count uint16)
   (chunks (vector log-chunk (bs:slot chunk-count))))
 
+(define-print-method log "~a ~d" (format-time (start-time object)) chunk-count)
+
 (bs:define-io-structure model
   (format uint8)
   (material-type uint8)
   (material-size uint32)
-  (face-count uint32)
   (textures (vector (string uint16) (ldb (byte 4 0) (bs:slot material-type))))
-  (face-indices (vector uint32 (bs:slot face-count)))
-  (vertices (vector float32)))
+  (faces (vector uint32 uint32) :offset (+ (bs:slot material-size) 6))
+  (vertices (vector float32 uint32)))
+
+(defun vertex-attributes (model)
+  (ecase (model-format model)
+    (#x03 '(:position))
+    (#x05 '(:position :uv))
+    (#x06 '(:position :color))
+    (#x16 '(:position :normal))
+    (#x08 '(:position :uv :normal))
+    (#x09 '(:position :color :normal))
+    (#x0B '(:position :uv :normal :tangent))
+    (#x0C '(:position :color :normal :tangent))))
+
+(defun texture-types (model)
+  (ecase (model-material-type model)
+    (#x00 '())
+    (#x01 '(:albedo))
+    (#x02 '(:albedo :normal))
+    (#x12 '(:albedo :emission))
+    (#x03 '(:albedo :normal :specular))
+    (#x13 '(:albedo :normal :emission))
+    (#x23 '(:albedo :normal :metallic))
+    (#x04 '(:albedo :normal :metalness :roughness))
+    (#x14 '(:albedo :normal :specular :emission))
+    (#x24 '(:albedo :normal :metallic :emission))
+    (#x05 '(:albedo :normal :metalness :roughness :emission))
+    (#x15 '(:albedo :normal :metalness :roughness :occlusion))
+    (#x06 '(:albedo :normal :metalness :roughness :occlusion :emission))))
+
+(define-print-method model "~a ~d" (vertex-attributes object) (length (model-vertices object)))
 
 (bs:define-io-structure text
   (encoding (case uint8
@@ -130,24 +198,34 @@
               (#x25 :gbk)))
   (text (string * (bs:slot encoding))))
 
+(define-print-methodm text "~a" (text-encoding text))
+
 (bs:define-io-structure color
   (r float32)
   (g float32)
   (b float32)
   (a float32))
 
+(define-print-method color "~,2f ~,2f ~,2f ~,2f" r g b a)
+
 (bs:define-io-structure point
   (x float32)
   (y float32))
+
+(define-print-method point "~,2f ~,2f" x y)
 
 (bs:define-io-structure size
   (w float32)
   (h float32))
 
+(define-print-method size "~,2f ~,2f" w h)
+
 (bs:define-io-structure line
   (color color)
   (thickness float32)
   (points (vector point uint16)))
+
+(define-print-method line "~f ~d" thickness (length (line-points object)))
 
 (bs:define-io-structure filled-shape
   (fill-color color)
@@ -159,20 +237,26 @@
   (location point)
   (size size))
 
+(define-print-method rectangle "~a ~a" location size)
+
 (bs:define-io-structure circle
   (:include filled-shape)
   (location point)
   (size size))
 
+(define-print-method circle "~a ~a" location size)
+
 (bs:define-io-structure polygon
   (:include filled-shape)
-  (location point)
   (points (vector point uint16)))
+
+(define-print-method polygon "~d" (length (polygon-points object)))
 
 (bs:define-io-structure curve
   (:include filled-shape)
-  (location point)
   (points (vector point uint16)))
+
+(define-print-method curve "~d" (length (curve-points object)))
 
 (bs:define-io-structure text-shape
   (color color)
@@ -180,6 +264,8 @@
   (font (string uint16))
   (font-size float32)
   (text (string uint16)))
+
+(define-print-method text-shape "~a ~a ~,2fun" location font font-size)
 
 (bs:define-io-structure vector-graphic
   (width uint32)
@@ -196,3 +282,5 @@
                                               '(1f0 0f0 0f0 0f0 1f0 0f0)))
                           (#x12 (vector float32 6))) 
                         (bs:slot count))))
+
+(define-print-method vector-graphic "~dx~d ~d" width height (length (vector-graphic-count object)))
