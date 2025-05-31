e3a83a106a78bdd14b5e2706b5b79a2704f0afa0
Yukari Hafner <shinmera@tymoon.eu>
2025-05-31 22:10:48 +0200

    Improve vector graphic constructors

--- vector-graphic.lisp
+++ vector-graphic.lisp
@@ -1,75 +1,127 @@
 (in-package #:org.shirakumo.sf3)
 
-(bs:define-io-structure color
+(bs:define-io-structure (color (:constructor %make-color))
   (r float32)
   (g float32)
   (b float32)
   (a float32))
 
+(defun make-color (r g b &optional (a 1.0))
+  (%make-color :r (float r 0f0)
+               :g (float g 0f0)
+               :b (float b 0f0)
+               :a (float a 0f0)))
+
 (define-print-method color "~,2f ~,2f ~,2f ~,2f" r g b a)
 
-(bs:define-io-structure point
+(bs:define-io-structure (point (:constructor %make-point))
   (x float32)
   (y float32))
 
+(defun make-point (x y)
+  (%make-point :x (float x 0f0)
+               :y (float y 0f0)))
+
 (define-print-method point "~,2f ~,2f" x y)
 
-(bs:define-io-structure size
+(bs:define-io-structure (size (:constructor %make-size))
   (w float32)
   (h float32))
 
+(defun make-size (w h)
+  (%make-size :w (float w 0f0)
+              :h (float h 0f0)))
+
 (define-print-method size "~,2f ~,2f" w h)
 
-(bs:define-io-structure shape-fill
+(bs:define-io-structure (shape-fill (:constructor %make-shape-fill))
   (fill-color color)
   (outline-color color)
   (outline-thickness float32))
 
-(bs:define-io-structure shape-bounds
+(defun make-shape-fill (&key (fill-color (make-color 0 0 0))
+                             (outline-color (make-color 0 0 0))
+                             (outline-thickness 0))
+  (%make-shape-fill :fill-color fill-color
+                    :outline-color outline-color
+                    :outline-thickness (float outline-thickness 0f0)))
+
+(bs:define-io-structure (shape-bounds (:constructor %make-shape-bounds))
   (point point)
   (size size))
 
-(bs:define-io-structure line
+(defun make-shape-bounds (x y w h)
+  (%make-shape-bounds :point (make-point x y) :size (make-size w h)))
+
+(bs:define-io-structure (line (:constructor %make-line))
   (points (vector point uint16))
   (color color)
   (thickness float32))
 
+(defun make-line (points &key (thickness 1) (color (make-color 0 0 0)))
+  (%make-line :points (coerce points 'vector)
+              :color color
+              :thickness (float thickness 0f0)))
+
 (define-print-method line "~f ~d" thickness (length (line-points object)))
 
-(bs:define-io-structure rectangle
+(bs:define-io-structure (rectangle (:constructor %make-rectangle))
   (bounds shape-bounds)
   (fill shape-fill))
+
+(defun make-rectangle (x y w h &rest fill-args)
+  (%make-rectangle :bounds (make-shape-bounds x y w h)
+                   :fill (apply #'make-shape-fill fill-args)))
 
 (define-print-method rectangle "~a ~a" location size)
 
-(bs:define-io-structure circle
+(bs:define-io-structure (circle (:constructor %make-circle))
   (bounds shape-bounds)
   (fill shape-fill))
 
+(defun make-circle (x y w h &rest fill-args)
+  (%make-circle :bounds (make-shape-bounds x y w h)
+                :fill (apply #'make-shape-fill fill-args)))
+
 (define-print-method circle "~a ~a" location size)
 
-(bs:define-io-structure polygon
+(bs:define-io-structure (polygon (:constructor %make-polygon))
   (points (vector point uint16))
   (fill shape-fill))
+
+(defun make-polygon (points &rest fill-args)
+  (%make-polygon :points (coerce points 'vector)
+                 :fill (apply #'make-shape-fill fill-args)))
 
 (define-print-method polygon "~d" (length (polygon-points object)))
 
-(bs:define-io-structure curve
+(bs:define-io-structure (curve (:constructor %make-curve))
   (points (vector point uint16))
   (fill shape-fill))
 
+(defun make-curve (points &rest fill-args)
+  (%make-curve :points (coerce points 'vector)
+               :fill (apply #'make-shape-fill fill-args)))
+
 (define-print-method curve "~d" (length (curve-points object)))
 
-(bs:define-io-structure text-shape
+(bs:define-io-structure (text-shape (:constructor %make-text-shape))
   (point point)
   (color color)
   (font (string uint16))
   (font-size float32)
   (text (string uint16)))
 
+(defun make-text-shape (x y text &key (color (make-color 0 0 0)) (font "sans-serif") (font-size 12))
+  (%make-text-shape :point (make-point x y)
+                    :color color
+                    :font font
+                    :font-size (float font-size 0f0)
+                    :text text))
+
 (define-print-method text-shape "~a ~a ~,2fun" location font font-size)
 
-(bs:define-io-structure vector-graphic
+(bs:define-io-structure (vector-graphic (:constructor %make-vector-graphic))
   (width uint32)
   (height uint32)
   (instructions (vector (case uint8
@@ -84,7 +136,10 @@
                           (#x12 (vector float32 6))) 
                         uint32)))
 
-(define-print-method vector-graphic "~dx~d ~d" width height (vector-graphic-count object))
+(defun make-vector-graphic (width height instructions)
+  (%make-vector-graphic :width width :height height :instructions (coerce instructions 'vector)))
+
+(define-print-method vector-graphic "~dx~d ~d" width height (length (vector-graphic-instructions object)))
 
 (define-accessors color r g b a)
 (define-accessors point x y)
